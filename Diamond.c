5ebded5287672e57ef5e5827294f5eb88e8ab2dc
cloudbix <cloudbi@proton.me>
2026-06-23 18:22:16 +0200

    Update Diamond.c

--- Diamond.c
+++ Diamond.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-void printDiamond(int n, char symbol) {
-    if (n % 2 == 0) n++; // Zapewnia parzystą szerokość
-    if (n <= 0) n = 2;   // Minimalna parzysta szerokość
-    for (int i = -n/2 ; i <= n/2 ; i++) { // Pomijamy ostatni wiersz, jeśli i == n/2
+void printDiamond(int n, char symbol) { 
+    for (int i = -n/2 ; i <= n/2 ; i++) { 
         int spaces = abs(i), stars = n - 2 * abs(i);
         while (spaces--) printf(" ");
         while (stars--) printf("%c", symbol);
@@ -15,10 +14,25 @@ void printDiamond(int n, char symbol) {
 int main() {
     int n;
     char symbol;
-    printf("Podaj szerokosc diamentu (parzysta): ");
-    scanf("%d", &n);
-    printf("Podaj znak (np. * lub #): ");
-    scanf(" %c", &symbol);
-    printDiamond(n, symbol);
+
+    while (1) {
+        printf("Podaj wymiar diamentu (nieparzysta dodatnia): ");
+        if (scanf("%d", &n) != 1 || n < 1 || n % 2 == 0) { 			            // walidacja dla n
+            printf("Podales niemozliwy wymiar!\n");
+            while (getchar() != '\n');  					                    // czyszczenie buforu
+            continue;
+        }
+
+        printf("Podaj znak (np. * lub #): ");
+        if (scanf(" %c", &symbol) != 1 || !isprint((unsigned char)symbol)) { 	// walidacja dla symbol
+            printf("Nie podales prawidlowego symbolu!\n");
+            while (getchar() != '\n');						                    // czyszczenie buforu
+            continue;
+        }
+
+        printDiamond(n, symbol);
+        break;
+    }
+
     return 0;
 }
